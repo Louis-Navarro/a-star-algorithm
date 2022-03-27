23a6f091a78233e483a15961bacfb6ff0efaeb76
azuiops.tv@gmail.com <azuiops.tv@gmail.com>
2022-03-27 23:50:37 +0200

    Added docstrings

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,5 @@
 from math import sqrt
 
-
 import numpy as np
 
 
@@ -8,20 +7,40 @@ class Algorithm:
     INF = float('INF')
 
     def __init__(self, shape, diag=True):
+        """The A* Algorithm is a path-finding algorithm, considered to be the most effective.
+        To use this implimentation, initialize the class with the dimensions (n*m) of the grid, and if the path can go in diagonal.
+        Then, run find_path() to find the optimal path.
+
+        Args:
+            shape (tuple): The shape of the grid
+            diag (bool, optional): Wether or not the algorithm can go in diagonal. Defaults to True.
+        """
+
         self.shape = shape
 
-        self.vis = np.full(shape, False)           # Keep track of visited nodes
-        self.dis = np.full(shape, np.inf)          # Distances of nodes to visit
+        # Keep track of visited nodes
+        self.vis = np.full(shape, False)
+        # Distances of nodes to visit
+        self.dis = np.full(shape, np.inf)
         self.parent = {}
 
-        if diag: # Eight neighbors
+        if diag:  # Eight neighbors
             self.dX = (-1, -1, 0, 1, 1, 1, 0, -1)
             self.dY = (0, 1, 1, 1, 0, -1, -1, -1)
-        else: # Four neighbors
+        else:  # Four neighbors
             self.dX = (-1, 0, 1, 0)
             self.dY = (0, -1, 0, 1)
 
     def find_path(self, grid, beg, end):
+        """Call this function to find the optimal path between the start point and the end point.
+        BE AWARE: THIS FUNCTION OVERWRITES THE GIVEN GRID, SO YOU MAY WANT TO CONSIDER COPYING THE GRID BEFORE CALLING THIS FUNCTION
+
+        Args:œ
+            grid (ndarray): Array representing the grid, containing a start point (1), an end point (2) and possibly walls (-1)
+            beg (Iterable): Iterable containing the coordinates of the starting point
+            end (Iterable): Iterable containing the coordinates of the ending point
+        """
+
         print("Exploring ...")
         self.explore(grid, beg, end)
         print("Backtracking ...")
@@ -29,35 +48,68 @@ class Algorithm:
         self.__init__(self.shape)
 
     def ok(self, a, b):
-        return a>=0 and a<self.shape[0] and b>=0 and b<self.shape[1]
+        """Check if a point of coordinate a and b is in the grid
+
+        Args:
+            a (int): Coordinate on the y-axis (line number)
+            b (int): Coordinate on the x-axis (column number)
+
+        Returns:
+            bool: Returns `true` if the point is in the grid, otherwise return `false`
+        """
+        return a >= 0 and a < self.shape[0] and b >= 0 and b < self.shape[1]
 
     @staticmethod
     def get_distance(cur, dest):
-        ans=sqrt(abs(cur[0]-dest[0])**2 + abs(cur[1]-dest[1])**2)
+        """Computes the distance between two points of the grid
+
+        Args:
+            cur (Iterable): List/tuple/ndarray representing the coordinates of the first point
+            dest (Iterable): List/tuple/ndarray representing the coordinates of the second point
+
+        Returns:
+            float: Returns the distance between the two points as a decimal number
+        """
+        ans = sqrt(abs(cur[0]-dest[0])**2 + abs(cur[1]-dest[1])**2)
         return ans
 
     def explore(self, grid, beg, end):
+        """Explores the grid and computes the score for each point between the starting and ending points.
+        These scores are then used to find the optimal paths between the two points.
+
+        Args:
+            grid (ndarray): Array representing the grid, containing a starting (1) and ending (2) points, and possibly walls (-1)
+            beg (Iterable): List/tuple/ndarray representing the coordinates of the first point
+            end (Iterable): List/tuple/ndarray representing the coordinates of the second point
+        """
         self.dis[beg[0], beg[1]] = self.get_distance(beg, end)
 
-        cur=beg
-        while cur!=end:
+        cur = beg
+        while cur != end:
             self.vis[cur[0], cur[1]] = True
-            p_dis=self.dis[cur[0], cur[1]] 
+            p_dis = self.dis[cur[0], cur[1]]
             self.dis[cur[0], cur[1]] = np.inf
 
             for k in range(len(self.dX)):
-                i=self.dX[k]
-                j=self.dY[k]
+                i = self.dX[k]
+                j = self.dY[k]
                 x, y = cur[0]+i, cur[1]+j
-                if self.ok(x, y) and self.dis[x, y]==np.inf and self.vis[x, y]==False and grid[x, y] != -1:
+                if self.ok(x, y) and self.dis[x, y] == np.inf and self.vis[x, y] == False and grid[x, y] != -1:
                     cur_dis = round(p_dis + self.get_distance(cur, (x, y)), 2)
                     if cur_dis < self.dis[x, y]:
                         self.dis[x, y] = cur_dis
                         self.parent[(x, y)] = cur
-            
+
             cur = np.unravel_index(np.argmin(self.dis, axis=None), self.shape)
 
     def backtrack(self, grid, beg, end):
+        """Finds the optimal path between the points
+
+        Args:
+            grid (ndarray): Array representing the grid, containing a starting (1) and ending (2) points, and possibly walls (-1)
+            beg (Iterable): List/tuple/ndarray representing the coordinates of the first point
+            end (Iterable): List/tuple/ndarray representing the coordinates of the second point
+        """
         cur = beg
         while cur != end:
             grid[cur[0], cur[1]] = 3
@@ -83,7 +135,6 @@ if __name__ == '__main__':
 
     algo = Algorithm((20, 20))
     algo.find_path(grid, (x1, y1), (x2, y2))
-    
+
     print('End grid :')
     print(grid)
-    
--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from algorithm import Algorithm
 import pygame as pg
 import numpy as np
 
+
 class Window:
     BACKGROUND_COLOR = (255, 255, 255)
     SEARCHED = False
 
     def __init__(self, width=500, height=500, square=10, title='A* Algorithm', win=None, fps=60):
+        """This class manages the pygame window and interactions with the window to draw the grid and use the algorithm.
+
+        Args:
+            width (int, optional): The width of the window (must be a multiple of `square`). Defaults to 500.
+            height (int, optional): The height of the window (must be a multiple of `square`). Defaults to 500.
+            square (int, optional): The size of a square in the grid. Defaults to 10.
+            title (str, optional): The title of the window. Defaults to 'A* Algorithm'.
+            win (pygame.Surface, optional): Initial window object. Defaults to None.
+            fps (int, optional): FPS at which the window runs. Defaults to 60.
+        """
         # General variables
         self.width = width
         self.height = height
@@ -26,14 +37,21 @@ class Window:
 
         # Pygame stuff
         pg.init()
-        self.win = win if win else pg.display.set_mode((width, height)) # Window
-        pg.display.set_caption(title) # Title
+        self.win = win if win else pg.display.set_mode(
+            (width, height))  # Window
+        pg.display.set_caption(title)  # Title
+        print(type(self.win))
 
         self.font = pg.font.SysFont('sourcecodepro', 5)
 
         self.clock = pg.time.Clock()
 
     def check_quit(self):
+        """Checks if the user wants to quit the application
+
+        Returns:
+            bool: Returns True if the user wants to quit the application, otherwise returns False
+        """
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.run = False
@@ -41,6 +59,11 @@ class Window:
         return False
 
     def draw_window(self, refresh):
+        """The program calls this function to draw the grid with the starting and ending points, borders, and the path if there it was computed
+
+        Args:
+            refresh (_type_): _description_
+        """
         self.win.fill(self.BACKGROUND_COLOR)
 
         # Wall lines
@@ -57,8 +80,10 @@ class Window:
         for wall in inx:
             y, x = wall
             x, y = x*self.square, y*self.square
-            if (x>0): x+=1
-            if (y>0): y+=1
+            if (x > 0):
+                x += 1
+            if (y > 0):
+                y += 1
             pg.draw.rect(self.win, 0, ((x, y), (self.square-1, self.square-1)))
 
         # Path
@@ -66,35 +91,46 @@ class Window:
         for path in inx:
             y, x = path
             x, y = x*self.square, y*self.square
-            if (x>0): x+=1
-            if (y>0): y+=1
-            pg.draw.rect(self.win, (0, 0, 255), ((x, y), (self.square-1, self.square-1)))
+            if (x > 0):
+                x += 1
+            if (y > 0):
+                y += 1
+            pg.draw.rect(self.win, (0, 0, 255),
+                         ((x, y), (self.square-1, self.square-1)))
 
         # Start
         y, x = self.start
         x, y = x*self.square, y*self.square
-        if (x>0): x+=1
-        if (y>0): y+=1
-        pg.draw.rect(self.win, (0, 255, 0), ((x, y), (self.square-1, self.square-1)))
+        if (x > 0):
+            x += 1
+        if (y > 0):
+            y += 1
+        pg.draw.rect(self.win, (0, 255, 0),
+                     ((x, y), (self.square-1, self.square-1)))
 
         # End
         y, x = self.end
         x, y = x*self.square, y*self.square
-        if (x>0): x+=1
-        if (y>0): y+=1
-        pg.draw.rect(self.win, (255, 0, 0), ((x, y), (self.square-1, self.square-1)))
+        if (x > 0):
+            x += 1
+        if (y > 0):
+            y += 1
+        pg.draw.rect(self.win, (255, 0, 0),
+                     ((x, y), (self.square-1, self.square-1)))
 
         if refresh:
             pg.display.flip()
 
     def check_click(self):
+        """Function that checks if the user pressed any mouse button, and if so, updates the grid accordingly
+        """
         pressed = pg.mouse.get_pressed()
         if any(pressed):
             position = pg.mouse.get_pos()
             row = position[1] // self.square
             col = position[0] // self.square
 
-            val=0
+            val = 0
             # Left click = start
             if pressed[0]:
                 self.start = row, col
@@ -108,22 +144,36 @@ class Window:
             elif pressed[2]:
                 self.end = row, col
 
-
     def check_keys(self):
+        """Function that check if the user pressed the escape key, and if so, clears the grid
+        """
         pressed = pg.key.get_pressed()
-        
+
         if not self.SEARCHED and pressed[pg.K_RETURN]:
             self.search()
 
         elif pressed[pg.K_ESCAPE]:
-            self.grid = np.zeros((self.width // self.square, self.height // self.square))
+            self.grid = np.zeros(
+                (self.width // self.square, self.height // self.square))
             self.SEARCHED = False
 
     def frame(self, quit=True, draw=True, click=True, keys=True):
+        """Function that is called every frame, and is responsible for the frame-to-frame action
+
+        Args:
+            quit (bool, optional): Wether or not the program should check if the user wants to quit. Defaults to True.
+            draw (bool, optional): Wether or not the program should draw the grid. Defaults to True.
+            click (bool, optional): Wether or not the program should check if the user pressed his mouse. Defaults to True.
+            keys (bool, optional): Wether or not the program should check if the user pressed the escape key on his keyboard. Defaults to True.
+
+        Returns:
+            int: Returns 0 if the user quit the program, otherwise does not return anything
+        """
         if self.fps:
             self.clock.tick(self.fps)
         if quit:
-            if self.check_quit(): return 0
+            if self.check_quit():
+                return 0
         if draw:
             self.draw_window(True)
         if click:
@@ -132,6 +182,8 @@ class Window:
             self.check_keys()
 
     def search(self):
+        """Function responsible for searching the optimal path between the points
+        """
         self.SEARCHED = True
         self.algo.find_path(self.grid, self.start, self.end)
 
